774f3ae36bdf831b4351d1ffc9b5ea35321c1341
Scott Campbell <stc9486@gmail.com>
2021-02-10 18:05:21 -0500

    Action interface

--- npm-app/src/state/reducers/packReducer.ts
+++ npm-app/src/state/reducers/packReducer.ts
@@ -4,13 +4,18 @@ interface PackState {
 	data: string[];
 }
 
-const reducer = (state: PackState, action: any) => {
+interface Action {
+	type: string;
+	payload?: any;
+}
+
+const packReducer = (state: PackState, action: Action): PackState => {
 	switch (action.type) {
 		case 'search_packs':
 			return { loading: true, error: null, data: [] };
-		case 'search_packs_success':
+		case 'search_success':
 			return { loading: false, error: null, data: action.payload };
-		case 'search_packs_error':
+		case 'search_error':
 			return { loading: false, error: action.payload, data: [] };
 
 		default:
