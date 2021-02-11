88a712d5663d1a62d7082494fac7789f8e2f9065
Scott Campbell <stc9486@gmail.com>
2021-02-10 20:01:39 -0500

    destructure actions w/ custom hook

--- npm-app/src/components/PackList.tsx
+++ npm-app/src/components/PackList.tsx
@@ -1,8 +1,19 @@
+import React, { useState } from 'react';
+import { useActions } from '../hooks/useActions';
+
 const PackList: React.FC = () => {
+	const [term, setTerm] = useState('');
+	const { searchPacks } = useActions();
+
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+
+		searchPacks(term);
+	};
 	return (
 		<div>
 			<form>
-				<input />
+				<input value={term} onChange={(e) => setTerm(e.target.value)} />
 				<button>Search</button>
 			</form>
 		</div>
