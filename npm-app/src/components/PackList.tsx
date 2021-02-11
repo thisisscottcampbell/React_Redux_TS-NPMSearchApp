a1cb06b81bd178b0e699b736228223cfc4f62a5d
Scott Campbell <stc9486@gmail.com>
2021-02-11 17:02:10 -0500

    done

--- npm-app/src/components/PackList.tsx
+++ npm-app/src/components/PackList.tsx
@@ -1,21 +1,27 @@
 import React, { useState } from 'react';
+import { useTypedSelector } from '../hooks/useTypedSelector';
 import { useActions } from '../hooks/useActions';
 
 const PackList: React.FC = () => {
 	const [term, setTerm] = useState('');
 	const { searchPacks } = useActions();
+	const { data, error, loading } = useTypedSelector((state) => state.packs);
 
 	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
 
 		searchPacks(term);
 	};
+
 	return (
 		<div>
-			<form>
+			<form onSubmit={handleSubmit}>
 				<input value={term} onChange={(e) => setTerm(e.target.value)} />
 				<button>Search</button>
 			</form>
+			{error && <h3>{error}</h3>}
+			{loading && <h3>Loading...</h3>}
+			{!error && !loading && data.map((name) => <div key={name}>{name}</div>)}
 		</div>
 	);
 };
